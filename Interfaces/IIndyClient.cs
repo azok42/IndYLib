764b0a85e3107c3b57d8531c1e834728d87835f1
Azok42 <anton.hackner@sz-ybbs.ac.at>
2026-06-06 11:53:22 +0200

    feat: add entry functions to interface

--- Interfaces/IIndyClient.cs
+++ Interfaces/IIndyClient.cs
@@ -6,6 +6,8 @@ namespace IndYLib.Interfaces;
 public interface IIndyClient
 {
    Task<Normal> MakeNormalEntryAsync(DateOnly date, int hour, string tid, string subject, string activity);
+   Task<Absence> MakeAbsenceEntryAsync(DateOnly date, int hour);
+   Task<SchoolEvent> MakeSchoolEventEntryAsync(DateOnly date, int hour, string tid, string description);
    Task<List<ValidDay>> GetValidDaysAsync(DateOnly startDate, DateOnly endDate);
    Task<List<Student>> GetStudentAsync();
    Task<List<Teacher>> GetTeachers();
