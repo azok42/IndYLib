1e437a28eb9228613505b7fbfb2f7360e53e2701
Azok42 <anton1.hackner@gmail.com>
2026-06-22 09:49:48 +0200

    docs(auth): document indy auth functions

--- Interfaces/IIndyAuth.cs
+++ Interfaces/IIndyAuth.cs
@@ -4,7 +4,26 @@ namespace IndYLib.Interfaces;
 
 public interface IIndyAuth
 {
+   /// <summary>
+   /// Create a new IndyClient class using an existing token.
+   /// </summary>
+   /// <param name="token">The preexisting token used to authenticate.</param>
+   /// <returns>A IIndyClient instance where the token is set.</returns>
    Task<IIndyClient> CreateClientAsync(Token token);
+
+   /// <summary>
+   /// Create a new IndyClient class using the user credentials to get a new token.
+   /// </summary>
+   /// <param name="username">The username of the user wanting to log in.</param>
+   /// <param name="password">The password of the user wanting to log in.</param>
+   /// <returns>A IIndyClient instance where the token is set.</returns>
    Task<IIndyClient> CreateClientAsync(string username, string password);
+
+   /// <summary>
+   /// Get a token using the user credentials.
+   /// </summary>
+   /// <param name="username">The username of the user wanting to log in.</param>
+   /// <param name="password">The password of the user wanting to log in.</param>
+   /// <returns>A new token object returned by the server.</returns>
    Task<Token> GetToken(string username, string password);
 }
