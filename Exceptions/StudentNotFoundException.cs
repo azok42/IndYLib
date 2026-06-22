5ac57d1654e1e0f7e6af5c66ff530c3439c32493
Azok42 <anton1.hackner@gmail.com>
2026-06-22 10:21:19 +0200

    refactor: replace string concatination with interpolation in excpetion message

--- Exceptions/StudentNotFoundException.cs
+++ Exceptions/StudentNotFoundException.cs
@@ -4,7 +4,7 @@ public class StudentNotFoundException : Exception
 {
    public StudentNotFoundException () : base("Student not found") { }
 
-   public StudentNotFoundException (string name) : base("Student '" + name + "' not found") { }
+   public StudentNotFoundException (string name) : base($"Student '{name}' not found") { }
 
-   public StudentNotFoundException (string name, Exception innerExcpetion) : base("Student '" + name + "' not found", innerExcpetion) { }
+   public StudentNotFoundException (string name, Exception innerExcpetion) : base($"Student '{name}' not found", innerExcpetion) { }
 }
