5fb4e5e40b9c449081fd475965e42b28d8117e26
Azok42 <anton1.hackner@gmail.com>
2026-06-22 10:17:51 +0200

    feat: add new constructor with innerExcpetion to StudentNotFoundException

--- Exceptions/StudentNotFoundException.cs
+++ Exceptions/StudentNotFoundException.cs
@@ -5,4 +5,6 @@ public class StudentNotFoundException : Exception
    public StudentNotFoundException () : base("Student not found") { }
 
    public StudentNotFoundException (string name) : base("Student '" + name + "' not found") { }
+
+   public StudentNotFoundException (string name, Exception innerExcpetion) : base("Student '" + name + "' not found", innerExcpetion) { }
 }
